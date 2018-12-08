9633acfb6f76f3e2797d65539071fde4b272c5a4
Viet Khang <vietkhangleho@gmail.com>
2018-12-08 16:50:07 +0100

    maj

--- creation/fixtures/attaques.js
+++ creation/fixtures/attaques.js
@@ -1,6 +1,9 @@
 const { createInsertQuery } = require('./_helper')
 const attaques = [`Triplattaque`, `Escalade`, `Morphing`, `Vœu`, `Grobisou`, `Pilonnage`, `Recyclage`, `Regard Médusant`, `E-Coque`, `Constriction`, `Picanon`, `Uppercut`, `Coup de Main`, `Flash`, `Conversion`, `Affûtage`, `Croc de Mort`, `Par Ici`, `Attrition`, `Force-Nature`, `Combo-Griffe`, `Explosion`, `Trempette`, `Exuviation`, `CoudKrâne`, `Météores`, `Relais`, `Rengorgement`, `BoulArmure`]
 const faker = require('faker')
+const { sample } = require('lodash')
+
+const { types } = require('./types')
 
 module.exports = {
 	attaques,
@@ -8,6 +11,7 @@ module.exports = {
 		return attaques.map((attaque, index) => {
 			return createInsertQuery('attaque', [
 				index,
+        types.indexOf(sample(types)),
 				`'${attaque}'`,
 				faker.random.number(250),
 				faker.random.number(100),
