4fd8a50dc48d521bac0f9ae4c71704c8282eb7b0
Your Name <you@example.com>
2018-12-10 10:47:39 +0100

    maj

--- letsgo.sh
+++ letsgo.sh
@@ -1,13 +1,31 @@
 #!/bin/sh
 . $(dirname "$0")/.ask.sh
 . $(dirname "$0")/.check.sh
+. $(dirname "$0")/.promptExecSql.sh
 clear
 cat README
 
-if ask "Voulez-vous régénérer la base ?"; then
-  if check node; then
-    cd src/creation && node . && cd ../../
-  fi
+if check node && ask "Régénérer les requêtes de création ?"; then
+  cd src/creation && node . && cd ../../
 fi
 
+clear
+cat README
+
 cp src/creation/bd.sql .
+
+find ./src/client/ -name 'nbMonstro'
+
+if check sqlplus; then
+  if ask "Reset de la base avec les requêtes de création ?"; then
+    ./.run.sh bd.sql
+  fi
+else
+  echo "Pas de sqlplus..."
+  exit
+fi
+
+while promptExecSql; do
+  :
+done
+
